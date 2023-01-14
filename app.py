64e4d42a968230e5b744ec95aefa5ab040268043
Hariprasath <49398090+hariprasath-v@users.noreply.github.com>
2023-01-14 11:25:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from matplotlib import pyplot as pyplot
 from git import Repo
 
 
-@st.cache(suppress_st_warning=True)
+@st.cache()
 def loadModel():
     start_dl = time.time()
     Repo.clone_from("https://github.com/WongKinYiu/yolov7",'yolov7')
